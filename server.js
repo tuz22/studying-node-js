afac99380a387bc1cb3ca08517d1561cb6ca3b80
tuz22 <seungji.dev@gmail.com>
2023-07-15 16:52:11 +0900

    auto increment

--- server.js
+++ server.js
@@ -44,11 +44,38 @@ app.get('/write', function(요청, 응답){
 /* post 요청 */
 app.post('/add', function(요청, 응답){ // input에 적은 정보는 '요청'에 저장되어있음. 쉽게 사용하려면 라이브러리(body-parser) 필요
     응답.send('전송완료')
-    console.log(요청.body.title);
     
-    db.collection('post').insertOne({title: 요청.body.title, date: 요청.body.date}, function(에러, 결과) {
-        console.log('저장완료');
-    })
+    // 1. DB.collection 내의 총 게시물갯수를 찾음
+    db.collection('counter').findOne({name: '게시물갯수'}, function(에러, 결과){
+        console.log(결과.totalPost)
+
+        // 2. 총 게시물갯수를 변수에 저장
+        var 총게시물갯수 = 결과.totalPost;
+
+        // 3. DB.collection에 새 게시물을 기록
+        db.collection('post').insertOne({ _id: 총게시물갯수 + 1, title: 요청.body.title, date: 요청.body.date}, function(에러, 결과) {
+            console.log('저장완료');
+            
+            // 4. counter란 collection에 이름이 게시물갯수인 것을 찾아서 totalPost란 항목을 1 증가시킴
+            // db.collection('counter').updateOne({어떤데이터를 수정할지}, {수정값}, function(){})
+            db.collection('counter').updateOne({name: '게시물갯수'}, { $inc : {totalPost: 1}}, function(에러, 결과){
+                if (에러) {
+                    return console.log(에러);
+                }
+            })
+        });
+
+        /* 
+            operator 
+            - $set : 변경
+            - $inc : 증가
+            - $min : 기존값보다 적을 때만 변경
+            - $rename : key값 이름 변경
+            ...
+        
+        */
+
+    });
 });
 
 /* HTML에 DB 데이터 넣기 (EJS) */
